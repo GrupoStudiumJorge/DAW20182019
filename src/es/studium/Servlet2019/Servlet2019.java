2155e6810a71326e4a175c52b9a48cbc59c887d8
Jairo <jairoinfinity@gmail.com>
2019-02-21 09:45:36 +0100

    Jairo Bernal

--- src/es/studium/Servlet2019/Servlet2019.java
+++ src/es/studium/Servlet2019/Servlet2019.java
@@ -37,5 +37,15 @@ public class Servlet2019 extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
+	
+	public int sumaFecha(String fecha)
+	{
+		String[] nums = fecha.split("/");
+		int dia = Integer.parseInt(nums[0]);
+		int mes = Integer.parseInt(nums[1]);
+		int anio = Integer.parseInt(nums[2]);
+		
+		return (dia + mes + anio);
+	}	
 
 }
