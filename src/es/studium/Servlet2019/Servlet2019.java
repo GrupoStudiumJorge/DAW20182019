8ae864ecf95ea97e37d952df5b7f2454179ed421
José Francisco Soto <jose_fran_soto@hotmail.com>
2019-02-21 09:54:10 +0100

    José Francisco Soto

--- src/es/studium/Servlet2019/Servlet2019.java
+++ src/es/studium/Servlet2019/Servlet2019.java
@@ -1,6 +1,7 @@
 package es.studium.Servlet2019;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -33,9 +34,18 @@ public class Servlet2019 extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
+	public String cambiarNombre(String cadena)
+	{
+		if(cadena != "")
+		{
+			cadena = "El Pera";
+		}
+		return cadena;
+	}
 }
