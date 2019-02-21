38468954c90c0314e674fbaaa88eb67afc0e8ed5
José Francisco Soto <jose_fran_soto@hotmail.com>
2019-02-21 10:43:31 +0100

    José Francisco Soto 5

--- src/es/studium/Servlet2019/Servlet2019.java
+++ src/es/studium/Servlet2019/Servlet2019.java
@@ -14,14 +14,14 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Servlet2019")
 public class Servlet2019 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Servlet2019() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Servlet2019() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -30,17 +30,18 @@ public class Servlet2019 extends HttpServlet {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+		/**
+		 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+		 */
+	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
+
 		doGet(request, response);
-		}
+
 	}
-	
+
 	//Luciano
 	public static String CambiarVocalesi(String cadena) {
 
@@ -54,22 +55,21 @@ public class Servlet2019 extends HttpServlet {
 		}
 		cadena = String.valueOf(cadenaCharArray);
 		return "Despues : " + cadena;
-}
-=======
-		// doGet(request, response);
-		
 	}
-	
+
+	// doGet(request, response);
+
+
+
 	public int sumaFecha(String fecha)
 	{
 		String[] nums = fecha.split("/");
 		int dia = Integer.parseInt(nums[0]);
 		int mes = Integer.parseInt(nums[1]);
 		int anio = Integer.parseInt(nums[2]);
-		
+
 		return (dia + mes + anio);
 	}	
->>>>>>> 5681e099d76e9bf37477c7ed249bed8bbbe10612
 
 
 	public String cambiarNombre(String cadena)
@@ -80,31 +80,31 @@ public class Servlet2019 extends HttpServlet {
 		}
 		return cadena;
 	}
-	
+
 	public int sacarVocales (String cadena)
 	{
-		 String vocales="aeiou";
-		 int resultadoContador = 0;
-        int contadorLetras[]={0,0,0,0,0};
- 
-        // recorremos la cadena recibida letra a letra
-        for(int i=0;i<cadena.length();i++)
-        {
-            // recorremos las vocales para comparar con cada una de las letras
-            for(int j=0;j<vocales.length();j++)
-            {
-                if(cadena.charAt(i)==vocales.charAt(j))
-                {
-                    // aumentamos el contador para la vocal encontrada
-                	contadorLetras[j]++;
-                }
-            }
-        }
-		
-        for(int a=0;a<vocales.length();a++)
-        {
-        	resultadoContador += contadorLetras[a];
-        }
+		String vocales="aeiou";
+		int resultadoContador = 0;
+		int contadorLetras[]={0,0,0,0,0};
+
+		// recorremos la cadena recibida letra a letra
+		for(int i=0;i<cadena.length();i++)
+		{
+			// recorremos las vocales para comparar con cada una de las letras
+			for(int j=0;j<vocales.length();j++)
+			{
+				if(cadena.charAt(i)==vocales.charAt(j))
+				{
+					// aumentamos el contador para la vocal encontrada
+					contadorLetras[j]++;
+				}
+			}
+		}
+
+		for(int a=0;a<vocales.length();a++)
+		{
+			resultadoContador += contadorLetras[a];
+		}
 		return resultadoContador;
 
 	}
